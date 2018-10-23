cb76c5641ab196dafa02481b5e07f1345e8798a2
qty <1273106281@qq.com>
2018-10-23 13:43:39 +0800

    '重量区间缩减'

--- src/main/java/com/mmall/service/serviceImpl/WeightCalculateServiceImpl.java
+++ src/main/java/com/mmall/service/serviceImpl/WeightCalculateServiceImpl.java
@@ -56,7 +56,8 @@ public class WeightCalculateServiceImpl extends ServiceImpl<WeightCalculateMappe
         WeightCalculate weightCalculate;
 
         //定义循环判断的规则
-        Double[] interval={0.01,0.5,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0,16.0,17.0,18.0,19.0,20.0};
+//        Double[] interval={0.01,0.5,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0,16.0,17.0,18.0,19.0,20.0};
+        Double[] interval={0.01,0.5,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0};
 
         List<Total> list = totalService.listToal(billParam.getDate(),billParam.getUserId());
         if(list.size()<=0){
@@ -82,11 +83,14 @@ public class WeightCalculateServiceImpl extends ServiceImpl<WeightCalculateMappe
         try {
             Field.setAccessible(fields, true);
             for (int i = 3; i < value.length; i++) {
+                if(i>14){
+                    break;
+                }
                 name[i] = fields[i].getName();
                 value[i] = fields[i].get(weightCalculate);
 
                 if(i==3){
-                    map.put("20以上",value[i]==null?"0":value[i].toString());
+                    map.put("10以上",value[i]==null?"0":value[i].toString());
                     continue;
                 }
 
